8362332690a3e6590c0db6746468460dd2338a05
Christian Lopez <clopez@fipasoft.mx>
2014-01-29 14:21:03 -0600

    FIX: Se borran los archivos antes del ciclo.

--- system/shutdown.sh
+++ system/shutdown.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 for archivo in $(find /tmp/ -maxdepth 1 -name shutdown-*); do
-    if [ $(($(date +%s)-${archivo#*-})) -gt 300 ]
+    rm $archivo
+    if [ $(($(date +%s)-${archivo#*-})) -lt 300 ]
     then
-        rm $archivo
-    else
-        rm $archivo
         shutdown now
     fi
 done
